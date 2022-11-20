a0abbb09a51669d67ce433fd2d59c945203671ed
Dawid Kostka <dpkostka@gmail.com>
2022-11-20 14:07:46 +0100

    new test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: "TSE2S2QZ7PhTbz0loAi9OQtt",
+  use: [apiPlugin],
+  components,
+});
+
+export default MyApp;
